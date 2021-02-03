28f42de049b78806cba299299e8ef9c7f3d5ebf3
Matthew Cook <matthewcook13987@gmail.com>
2021-02-03 23:39:42 +0000

    Added instructions for function completion.

--- letterPositions.js
+++ letterPositions.js
@@ -1,19 +1,16 @@
 /*
-return all the indices (zero-based positions) 
+return all the indices (zero-based positions)
 in the string where each character is found.
 
-For each letter, instead of returning just one number to represent its number of occurrences, 
+For each letter, instead of returning just one number to represent its number of occurrences,
 multiple numbers may be needed to represent all the places in the string that it shows up.
 */
-
 
 const letterPositions = function(sentence) {
   const results = {};
   // logic to update results here
   return results;
 };
-
-//{ L: [0], i: [1,9], g: [2], h: [3,5,13,15], t: [4], o: [6,16], u: [7,17], s: [8,18], e: [9,14,19], n: [11] }
 
 
 const eqArrays = function(arr1, arr2) {

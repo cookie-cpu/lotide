6eaff9226425bcf07ed4896e778fd4eee388e318
Matthew Cook <matthewcook13987@gmail.com>
2021-02-03 22:14:40 +0000

    Initial commit with assertEqual function added.

---
+++ countLetters.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed😀: ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😟Assertion Failed😟: ${actual} !===  ${expected}`);
+  }
+};
+

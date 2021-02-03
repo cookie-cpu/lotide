0658de23ce161ee81b3d8f592bcaf58aadb22510
Matthew Cook <matthewcook13987@gmail.com>
2021-02-03 22:13:46 +0000

    First and final commit for countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -6,6 +6,45 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countOnly = function(params) {
-  
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+  let count = 0;
+
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+
 }
+
+
+
+//Test Cases
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);

b8724f8dda01898d9d013d079c58ddc9a81e3826
Matthew Cook <matthewcook13987@gmail.com>
2021-02-03 23:11:47 +0000

    Added conditional to filter out space before looping through each letter and first assigning them to a key before incrementing their associated value.

--- countLetters.js
+++ countLetters.js
@@ -6,11 +6,48 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const countLetters = function(sentence) {
-  for (let letter in sentence) {
-    console.log(sentence[letter]);
-  }
-  
+const countLetters = function(word) {
+  const output = {};
+  for (let letter in word) {
+    if (word[letter] !== " ") {
+      if (output[word[letter]]) {
+        output[word[letter]] += 1;
+      } else {
+        output[word[letter]] = 1;
+      }
+    }
+  } return output;
 };
+//{ L: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 }
 
-console.log(countLetters("Lighthouse"));
+assertEqual(countLetters("Lighthouse in the house"),{
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+});
+
+
+console.log(countLetters("Lighthouse in the house"));
+
+
+// const countOnly = function(allItems, itemsToCount) {
+//   const results = {};
+//   for (const item of allItems) {
+//     if (itemsToCount[item]) {
+//       if (results[item]) {
+//         results[item] += 1;
+//       } else {
+//         results[item] = 1;
+//       }
+//     }
+//   }
+//   return results;
+// };
+

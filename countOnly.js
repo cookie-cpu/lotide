5418931fa46436a576f919962c30b71dce7579dd
Matthew Cook <matthewcook13987@gmail.com>
2021-02-03 22:01:01 +0000

    Initial commit of countOnly.js with assertEqual function and countOnly declarations.

---
+++ countOnly.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed😀: ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😟Assertion Failed😟: ${actual} !===  ${expected}`);
+  }
+};
+
+const countOnly = function(params) {
+  
+}

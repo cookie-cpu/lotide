a63153de754039a620f5e06b26c3a9b998e32178
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 22:15:45 +0000

    added test cases to test functionality.

--- map.js
+++ map.js
@@ -1,16 +1,3 @@
-const words = ["ground", "control", "to", "major", "tom"];
-
-const map = function(arr,func){
-  const results = [];
-  for (let item of arr){
-    results.push(func(item))
-  }
-  return results;
-}
-
-const results1 = map(words, word => word[0]);
-console.log(results1);
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i ++) {
@@ -31,3 +18,23 @@ const assertArraysEqual = function(actual,expected) {
     console.log(`😟Assertion Failed😟: ${actual} !===  ${expected}`);
   }
 };
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+const words2 = ["this", "is", "my", "test", "array"];
+
+const map = function(arr,func){
+  const results = [];
+  for (let item of arr){
+    results.push(func(item))
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+const result2 = map(words2, word => word[word.length-1]);
+console.log(result2)
+
+assertArraysEqual(map(words2, word => word[word.length-1]), ["s,s,y,t,y"])
+assertArraysEqual(map(words2, word => word[0]), ["t,i,m,t,a"])

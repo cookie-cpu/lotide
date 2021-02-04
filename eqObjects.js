3db69b0fa9a50670ae80e333163cece700560dda
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 01:44:36 +0000

    added test cases

--- eqObjects.js
+++ eqObjects.js
@@ -10,5 +10,16 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise return false!
 const eqObjects = function(object1, object2) {
-  
+
 };
+
+//TEST CASES
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+//eqObjects(ab, ba); // => true
+assertEqual(eqObjects(ab, ba),true)
+
+
+const abc = { a: "1", b: "2", c: "3" };
+//eqObjects(ab, abc); // => false
+assertEqual(eqObjects(ab, abc), false)

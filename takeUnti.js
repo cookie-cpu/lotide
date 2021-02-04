2dd00bf9bccafdee8ce385ae948c7c20ffa65a4c
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 23:00:00 +0000

    Changed the function logic to incorporate the truthy/falsey values of the callback function into the conditional.

--- takeUnti.js
+++ takeUnti.js
@@ -1,13 +1,12 @@
 const takeUntil = function(array, callback){
   let output = [];
   for (let item in array){
-    if (!array[item] === false){
+    if (!callback(array[item])){
       output.push(array[item])
+    } else if (callback(array[item])){
+      return output
     }
   } return output
-  //Collects item from array until callback returns true
-  //Returns array
-
 }
 
 

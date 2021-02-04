9a273335e20879fa3465814906e6e388c045ec45
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 22:08:17 +0000

    Rewrote function logic to remove debug logs and complete logic for program.

--- map.js
+++ map.js
@@ -1,7 +1,11 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(arr,func){
-
+  const results = [];
+  for (let item of arr){
+    results.push(func(item))
+  }
+  return results;
 }
 
 const results1 = map(words, word => word[0]);

46f03a1e8b534db23c807ee491ecb73183691f45
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 02:00:32 +0000

    copied eqArrays.js over to add array filtering to eqObjects.

--- eqObjects.js
+++ eqObjects.js
@@ -15,16 +15,25 @@ const eqObjects = function(object1, object2) {
   if (keys2.length !== keys1.length){
     return false
   }
-  
-  
-  
-  // console.log(keys1)
-  // console.log()
-  // console.log(keys2)
-
+  for (let key in object1){
+    if (object1[key] !== object2[key]){
+      return false
+    }
+  } return true;
 };
 
-
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i ++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
 
 
 //TEST CASES
@@ -36,3 +45,11 @@ assertEqual(eqObjects(ab, ba),true)
 const abc = { a: "1", b: "2", c: "3" };
 //eqObjects(ab, abc); // => false
 assertEqual(eqObjects(ab, abc), false)
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);

d34dd99b9cbcccfeea8982b28c3bca2c8d3c3ca2
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 22:08:47 +0000

    copied over assertArraysEqual to test functionality of program.

--- map.js
+++ map.js
@@ -11,3 +11,23 @@ const map = function(arr,func){
 const results1 = map(words, word => word[0]);
 console.log(results1);
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i ++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`😀Assertion Passed😀: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`😟Assertion Failed😟: ${actual} !===  ${expected}`);
+  }
+};

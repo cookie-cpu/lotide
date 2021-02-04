a88b9de53ad952695a87c0f7a6e581e90e2108eb
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 23:24:34 +0000

    Added in instructions and pseudocode.

--- findKey.js
+++ findKey.js
@@ -5,3 +5,15 @@ const assertEqual = function(actual, expected) {
     console.log(`😟Assertion Failed😟: ${actual} !===  ${expected}`);
   }
 };
+
+/*
+Implement the function findKey which takes in an object and a callback.
+ It should scan the object and return the first key for which the callback returns a truthy value.
+  If no key is found, then it should return undefined.
+*/
+
+const findKey = function(object, callback){
+//loop through obj
+//if callback = truthy return key
+//no key = undefined
+}

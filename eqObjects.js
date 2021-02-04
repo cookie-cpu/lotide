18c7fb34c48a99f6213ba4632ca3d36e263a0be4
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 01:39:47 +0000

    initial commit

---
+++ eqObjects.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀Assertion Passed😀: ${actual} ===  ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😟Assertion Failed😟: ${actual} !===  ${expected}`);
+  }
+};

b272665313fbd3b3f327446f80e3e47ffa6de990
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 01:49:04 +0000

    created keys var to track object keys and loop through the items.

--- eqObjects.js
+++ eqObjects.js
@@ -10,15 +10,22 @@ const assertEqual = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise return false!
 const eqObjects = function(object1, object2) {
+  let keys1 = Object.keys(object1);
+  let keys2 = Object.keys(object2);
+  console.log(keys1)
+  console.log()
+  console.log(keys2)
 
 };
+
+
+
 
 //TEST CASES
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 //eqObjects(ab, ba); // => true
 assertEqual(eqObjects(ab, ba),true)
-
 
 const abc = { a: "1", b: "2", c: "3" };
 //eqObjects(ab, abc); // => false

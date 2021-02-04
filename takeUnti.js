ba86a35c8a043c16daa36e832b79e77aa46d00cd
Matthew Cook <matthewcook13987@gmail.com>
2021-02-04 23:02:52 +0000

    Copied asserArraysEqual over to better test the functionality of the program and ensure it is working as intended.

--- takeUnti.js
+++ takeUnti.js
@@ -13,13 +13,13 @@ const takeUntil = function(array, callback){
 //TEST CASES
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+// console.log(results1);
 
-console.log('---');
+// console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// console.log(results2);
 
 
 /* Expected Output
@@ -27,3 +27,27 @@ console.log(results2);
 ---
 [ 'I\'ve', 'been', 'to', 'Hollywood' ]
 */
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i ++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual,expected) {
+  if (eqArrays(actual,expected)) {
+    console.log(`😀Assertion Passed😀: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`😟Assertion Failed😟: ${actual} !===  ${expected}`);
+  }
+};
+
+assertArraysEqual(results1,[ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
